6273a1911728d43da9b3f26f67c83b5f688cb70b
AOMuiz <54303765+AOMuiz@users.noreply.github.com>
2020-08-21 11:57:17 +0100

    Updates

---
+++ precache-manifest.a76f534bf95c628c7d68dfd2bec7575a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e2e43abff140ae1baf8a03c9dbf451",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "c96a0f14386391e33638",
+    "url": "/React-Calculator/static/css/main.c309e169.chunk.css"
+  },
+  {
+    "revision": "1afe26061f3870f832c9",
+    "url": "/React-Calculator/static/js/2.ff4c4915.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/React-Calculator/static/js/2.ff4c4915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c96a0f14386391e33638",
+    "url": "/React-Calculator/static/js/main.d2f580a2.chunk.js"
+  },
+  {
+    "revision": "c9dfe1fc53f9fd21c69a",
+    "url": "/React-Calculator/static/js/runtime-main.ff386b7a.js"
+  }
+]);
